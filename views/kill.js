91b36ba2ee5e2470ade1fb8e3bbb154b7dd104dc
poroshyndd <poroshyndd@gmail.com>
2025-04-03 12:57:12 +0200

    Update kill.js

--- views/kill.js
+++ views/kill.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const { getProcessLog } = require("../utils/logger");
+
+router.get("/", (req, res) => {
+  getProcessLog("Zamykanie procesu aplikacji na żądanie /kill");
+
+  res.send("Serwer został zatrzymany.");
+  
+  setTimeout(() => {
+    process.exit();
+  }, 100);
+});
+
+module.exports = router;
 
