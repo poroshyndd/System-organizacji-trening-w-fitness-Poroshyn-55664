f127dde79e81472d57d814a230b8482e318e66d7
poroshyndd <poroshyndd@gmail.com>
2025-04-03 13:04:55 +0200

    Update renderNewProductPage.js

--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,5 +1,31 @@
 /* 🏗 Structo the Builder */
 /* Funkcja generująca stronę HTML z najnowszym produktem */
-const renderNewProductPage = () => {};
+const renderNewProductPage = (productData, hasError) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Shop - Newest product</title>
+    </head>
+    <body>
+      <h1>Newest product</h1>
+      <nav>
+        <a href="/">Home</a><br />
+        <a href="/product/add">Add product</a><br />
+        <a href="/logout">Logout</a>
+      </nav>
+      <main>
+        ${
+          hasError
+            ? "<br /><div>No new products available.</div>"
+            : `<br /><div>New product data - ${productData}</div>`
+        }
+      </main>
+    </body>
+    </html>
+  `;
+};
 
 module.exports = renderNewProductPage;
