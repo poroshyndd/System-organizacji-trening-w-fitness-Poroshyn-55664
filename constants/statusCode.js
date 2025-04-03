6283f0f67526bfdc8e81c7e8add7e8aef1982183
poroshyndd <poroshyndd@gmail.com>
2025-04-03 13:06:03 +0200

    Update statusCode.js

--- constants/statusCode.js
+++ constants/statusCode.js
@@ -3,4 +3,13 @@ const STATUS_CODE = {
   NOT_FOUND: 404,
 };
 
-module.exports = { STATUS_CODE };
+module.exports = {
+  STATUS_CODE: {
+    OK: 200,
+    FOUND: 302,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    BAD_REQUEST: 400,
+  },
+};
+
